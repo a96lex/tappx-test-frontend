42f708ebe0f2a47687affd9689ec70b71eb2a434
a96lex <a96lex@gmail.com>
2021-08-01 21:05:10 +0200

    add some theming

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -2,9 +2,11 @@ export const Theme = {
   primary: "#ff003c",
   primaryLight: "#ff5273",
   secondary: "#1f383f",
+  secondaryLight: "#f7f7f7",
   body: "#1e282c",
   bodyLight: "#929292",
   background: "#fafafa",
   backgroundLight: "#ffffff",
+  shadow: "0px 27.96px 37.28px rgba(50, 50, 71, 0.08)",
   fonts: { small: "12px", medium: "16px", large: "24px" },
 };
