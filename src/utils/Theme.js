c12ae03125ffa056f58d42f1e076f973f11329b9
a96lex <a96lex@gmail.com>
2021-08-01 18:03:55 +0200

    add theme

---
+++ src/utils/Theme.js
@@ -0,0 +1,9 @@
+export const Theme = {
+  primary: "#ff003c",
+  primaryLight: "#ff5273",
+  secondary: "#1f383f",
+  body: "#1e282c",
+  bodyLight: "#929292",
+  background: "#ffffff",
+  fonts: { small: "12px", medium: "16px", large: "24px" },
+};
