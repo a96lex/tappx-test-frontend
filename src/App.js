2c0ab49cf85a85ff59f308411a3d9cb4ed881718
a96lex <a96lex@gmail.com>
2021-08-01 19:54:18 +0200

    simplified app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,5 @@ import React from "react";
 import { Main } from "./screens";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+  return <Main />;
 }
