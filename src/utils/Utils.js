36bc36f73f6bb804619e8020b61c5764754d1aaa
a96lex <a96lex@gmail.com>
2021-08-01 21:05:03 +0200

    test data

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,1 +1,28 @@
 export const widthTreshold = "900px";
+
+export const helperData = [
+  {
+    name: "bundle1",
+    bundle: "a.a.a",
+    company: "alexcommany",
+    email: "alex@email.com",
+    active: true,
+    category: "tool",
+  },
+  {
+    name: "bundle2",
+    bundle: "a.a.a",
+    company: "alexcommany",
+    email: "alex@email.com",
+    active: true,
+    category: "tool",
+  },
+  {
+    name: "bundle3",
+    bundle: "a.a.a",
+    company: "alexcommany",
+    email: "alex@email.com",
+    active: true,
+    category: "tool",
+  },
+];
