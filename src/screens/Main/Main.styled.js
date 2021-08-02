5c68af6ab0d71791dbdcae2bc13db2a2681beda7
a96lex <a96lex@gmail.com>
2021-08-02 16:56:57 +0200

    simplified style

--- src/screens/Main/Main.styled.js
+++ src/screens/Main/Main.styled.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 import { Theme } from "../../utils";
 
-const headerOffset = 70;
+const headerOffset = "70px";
 
 export const AppLayout = styled.div`
   width: 100vw;
-  min-height: calc(100vh - ${headerOffset}px);
+  min-height: calc(100vh - ${headerOffset});
   background-color: ${Theme.background};
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: ${headerOffset}px;
+  padding-top: ${headerOffset};
 `;
 
 export const MainCard = styled.div`
