ac5b297ef04e3c617bda4401d3bf52b8d9232754
a96lex <a96lex@gmail.com>
2021-08-06 13:33:29 +0200

    add signature

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -4,12 +4,18 @@ import { Theme } from "../../utils";
 export const StyledFooter = styled.div`
   background-color: ${Theme.body};
   width: 100vw;
+  height: ${Theme.footerHeight};
   display: flex;
   flex-wrap: auto;
   align-items: center;
+  flex-direction: column;
   justify-content: space-around;
-  height: ${Theme.footerHeight};
   div {
+    width: 100%;
+    display: flex;
+    flex-wrap: auto;
+    align-items: center;
+    justify-content: space-around;
     color: ${Theme.background};
   }
   z-index: 0;
