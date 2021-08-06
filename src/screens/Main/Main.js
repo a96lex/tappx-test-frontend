49cb5acb4fff1f7b866eba55826be869750657f8
a96lex <a96lex@gmail.com>
2021-08-06 11:03:22 +0200

    get bundle list on load

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BundleManagement, Footer, Header } from "../../components";
 import { AppLayout, MainCard } from "./Main.styled";
+import { useBundleActions } from "../../context";
 
 export function Main() {
+  const { getBundles } = useBundleActions();
+
+  useEffect(() => {
+    getBundles();
+  }, []);
+
   return (
     <>
       <Header />
