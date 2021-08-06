725299d687c26aeb9d62ea6b09316db5c5ce02b0
a96lex <a96lex@gmail.com>
2021-08-06 10:57:20 +0200

    error checking, separated components

--- src/components/BundleModal/BundleModal.js
+++ src/components/BundleModal/BundleModal.js
@@ -1,15 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { useBundleActions, useBundleState } from "../../context";
+import { bundleRegex, emailRegex } from "../../utils";
 import { MainButton } from "../MainButton";
 import { Modal } from "../Modal";
 
+const categoryOptions = ["tool", "music", "game", "social"];
+
 export function BundleModal() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [bundle, setBundle] = useState("");
-  const [company, setCompany] = useState("");
-  const [active, setActive] = useState("");
-  const [category, setCategory] = useState("");
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [bundle, setBundle] = useState("");
+  // const [company, setCompany] = useState("");
+  // const [active, setActive] = useState(0);
+  // const [category, setCategory] = useState(categoryOptions[0]);
+  const [name, setName] = useState("alex");
+  const [email, setEmail] = useState("alex@loves.cris");
+  const [bundle, setBundle] = useState("alex.loves.cris");
+  const [company, setCompany] = useState("alex");
+  const [active, setActive] = useState(0);
+  const [category, setCategory] = useState(categoryOptions[0]);
+
+  const [nameError, setNameError] = useState(null);
+  const [emailError, setEmailError] = useState(null);
+  const [bundleError, setBundleError] = useState(null);
+  const [companyError, setCompanyError] = useState(null);
 
   const [showDelete, setShowDelete] = useState(false);
 
@@ -17,7 +31,8 @@ export function BundleModal() {
   const { selectedBundle, isCreation } = useBundleState();
 
   useEffect(() => {
-    if (selectedBundle) {
+    if (selectedBundle && !isCreation) {
+      console.log({ isCreation });
       setName(selectedBundle.name);
       setEmail(selectedBundle.email);
       setBundle(selectedBundle.bundle);
@@ -32,21 +47,95 @@ export function BundleModal() {
     unSelectBundle();
   };
 
+  const checkErrors = () => {
+    let nameCheck = "";
+    let emailCheck = "";
+    let bundleCheck = "";
+    let companyCheck = "";
+
+    if (name == "") {
+      nameCheck = "Please enter a name";
+    }
+    if (!emailRegex.test(email)) {
+      emailCheck = "Please check your email adress";
+    }
+    if (!bundleRegex.test(bundle)) {
+      bundleCheck = "Please check your bundle";
+    }
+    if (company == "") {
+      companyCheck = "Please enter a company";
+    }
+    setNameError(nameCheck);
+    setEmailError(emailCheck);
+    setBundleError(bundleCheck);
+    setCompanyError(companyCheck);
+
+    return nameCheck || emailCheck || bundleCheck || companyCheck;
+  };
+
+  const handleCreate = () => {
+    const error = checkErrors();
+    console.log({ error, nameError });
+    if (!checkErrors()) {
+      console.log("no errores");
+      createBundle({
+        name,
+        email,
+        bundle,
+        company,
+        active,
+        category,
+      });
+    }
+  };
+
   return (
     <Modal close={handleClose} show={selectedBundle}>
       <h2>{isCreation ? "Introduce bundle data" : "Bundle details"}</h2>
-      <BundleInput label="Name:" value={name} setValue={setName} />
-      <BundleInput label="Email:" value={email} setValue={setEmail} />
-      <BundleInput label="Bundle:" value={bundle} setValue={setBundle} />
-      <BundleInput label="Company:" value={company} setValue={setCompany} />
-      <BundleInput label="Active:" value={active} setValue={setActive} />
-      <BundleInput label="Category:" value={category} setValue={setCategory} />
+      <BundleInputText
+        label="Name:"
+        value={name}
+        setValue={setName}
+        error={nameError}
+        setError={setNameError}
+      />
+      <BundleInputText
+        label="Email:"
+        value={email}
+        setValue={setEmail}
+        error={emailError}
+        setError={setEmailError}
+      />
+      <BundleInputText
+        label="Bundle:"
+        value={bundle}
+        setValue={setBundle}
+        error={bundleError}
+        setError={setBundleError}
+      />
+      <BundleInputText
+        label="Company:"
+        value={company}
+        setValue={setCompany}
+        error={companyError}
+        setError={setCompanyError}
+      />
+      <BundleInputCheckbox
+        label="Active:"
+        value={active}
+        setValue={setActive}
+      />
+
+      <BundleInputDropdown
+        label="Category:"
+        value={category}
+        setValue={setCategory}
+        options={categoryOptions}
+      />
       <MainButton
         secondary={!isCreation}
         onClick={() =>
-          isCreation
-            ? createBundle({ name, email, bundle, company, active, category })
-            : setShowDelete(!showDelete)
+          isCreation ? handleCreate() : setShowDelete(!showDelete)
         }
       >
         {isCreation ? "CREATE" : showDelete ? "CANCEL" : "DELETE"}
@@ -60,15 +149,58 @@ export function BundleModal() {
   );
 }
 
-function BundleInput({ label, value, setValue }) {
+function BundleInputText({ label, value, setValue, error = "", setError }) {
   return (
-    <label>
-      {label}
+    <>
+      <div>
+        {label} <span style={{ color: "red" }}>{error}</span>
+      </div>
       <input
         type="text"
         value={value}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={(e) => {
+          setValue(e.target.value);
+          setError && setError(null);
+        }}
       />
-    </label>
+    </>
+  );
+}
+
+function BundleInputCheckbox({ label, value, setValue }) {
+  return (
+    <>
+      <div>{label}</div>
+      <input
+        type="checkbox"
+        value={value}
+        onChange={() => {
+          setValue(Math.abs(value - 1));
+        }}
+      />
+    </>
+  );
+}
+
+function BundleInputDropdown({ label, value, setValue, options = [] }) {
+  return (
+    <>
+      <div>{label}</div>
+      <select
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        style={{ textTransform: "capitalize" }}
+      >
+        {options.map((option, index) => (
+          <option
+            name={option}
+            key={index}
+            style={{ textTransform: "capitalize" }}
+          >
+            {option}
+          </option>
+        ))}
+      </select>
+    </>
   );
 }
